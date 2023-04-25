05393dee66adaa3b334479fa3770bd6d58cb62ed
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-04-25 11:35:14 +0200

    Commentaires des fonctions

--- appvisulsl/src/appvisulsl/app.py
+++ appvisulsl/src/appvisulsl/app.py
@@ -2,19 +2,18 @@
 My first application
 """
 import asyncio
+import os
+
+import matplotlib.pyplot as plt
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-import matplotlib.pyplot as plt
-import os
-from sys import platform
 
 listeString = ["Il est tard mon ami"]
 
 
 class HelloWorld(toga.App):
-    nbGraphes = 0
-    heure = None
+    nbGraphes = 0 # number of generated graph
     valeurDict = 1
     dictVal = {1: {
         'labels': ['Frogs', 'Hogs', 'Dogs', 'Logs'],
@@ -38,15 +37,15 @@ class HelloWorld(toga.App):
         }}
 
     def createData(self):
-        # make a pie chart
+        """
+        Generate pie chart
+        """
         plt.pie(HelloWorld.dictVal[HelloWorld.valeurDict]['sizes'],
                 labels=HelloWorld.dictVal[HelloWorld.valeurDict]['labels'])
         try:
-            save = os.path.join(os.path.normpath(toga.App.app.paths.app),str(HelloWorld.nbGraphes) + ".png")
+            save = os.path.join(os.path.normpath(toga.App.app.paths.app), str(HelloWorld.nbGraphes) + ".png")
             try:
                 self.main_box.remove(self.imageChart)
-                self.imageChart = None
-                self.image = None
             except:
                 pass
             plt.savefig(save)
@@ -57,7 +56,7 @@ class HelloWorld(toga.App):
                 HelloWorld.valeurDict = 1
             plt.close()
         except:
-            save = os.path.join(os.path.normpath(toga.App.app.paths.app),str(HelloWorld.nbGraphes) + ".png")
+            save = os.path.join(os.path.normpath(toga.App.app.paths.app), str(HelloWorld.nbGraphes) + ".png")
             try:
                 self.main_box.remove(self.imageChart)
                 self.imageChart = None
@@ -73,11 +72,14 @@ class HelloWorld(toga.App):
             plt.close()
 
     def afficherGraphe(self, widget):
+        """
+        Display the graph
+        """
         self.createData()
         try:
-            save = os.path.join(os.path.normpath(toga.App.app.paths.app), str(HelloWorld.nbGraphes-1) + ".png")
+            save = os.path.join(os.path.normpath(toga.App.app.paths.app), str(HelloWorld.nbGraphes - 1) + ".png")
         except:
-            save = os.path.join(os.path.normpath(toga.App.app.paths.app), str(HelloWorld.nbGraphes-1) + ".png")
+            save = os.path.join(os.path.normpath(toga.App.app.paths.app), str(HelloWorld.nbGraphes - 1) + ".png")
         self.image = toga.Image(save)
         self.imageChart = toga.ImageView(id='view1', image=self.image)
         self.main_box.add(self.imageChart)
@@ -101,6 +103,9 @@ class HelloWorld(toga.App):
     #     self.add_background_task(self.regenGraphe)
 
     def startup(self):
+        """
+        Construct and show the Toga application.
+        """
         self.main_box = toga.Box(style=Pack(direction=COLUMN))
         name_label = toga.Label(
             "Your name: ",
@@ -125,9 +130,16 @@ class HelloWorld(toga.App):
             style=Pack(padding=5)
         )
 
+        buttonQuit = toga.Button(
+            "Leave",
+            on_press=self.closeTelephone,
+            style=Pack(padding=5)
+        )
+
         self.main_box.add(self.labelhttpx)
         self.main_box.add(name_box)
         self.main_box.add(button)
+        self.main_box.add(buttonQuit)
         self.main_box.add(self.boutonChart)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
@@ -138,22 +150,49 @@ class HelloWorld(toga.App):
         self.app.on_exit = self.fermerture
 
     def say_hello(self, widget):
+        """
+        Respond to the click of the button by updating the label
+        """
         self.main_window.info_dialog(
             greeting(self.name_input.value),
             "Hi there!",
         )
 
     def changeTitle(self, widget):
+        """
+        Change the title of the window after 3 seconds
+        """
         yield 3
         self.main_window.title = "Il est tard"
 
     async def changeTrueTitle(self, widget):
+        """
+        Change again the title of the window after 10 seconds
+        """
         await asyncio.sleep(10)
         self.main_window.title = "Il est vraiment tard"
 
     def fermerture(self, widget):
-        print("Au revoir")Aj
+        """
+        Function executing when the app is closing
+        """
+        print("Au revoir")
+        save = os.path.normpath(toga.App.app.paths.app)
+        i = 0
+        for file in os.listdir(save):
+            if (file == str(i) + ".png"):
+                os.remove(os.path.join(os.path.normpath(toga.App.app.paths.app), file))
+                i += 1
+            if i > HelloWorld.nbGraphes:
+                break
         return True
+
+    def closeTelephone(self, widget):
+        """
+        Test function
+        """
+        print("Au revoir")
+
 
 def greeting(name):
     if name:
