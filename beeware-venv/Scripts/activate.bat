9f1a8c051c1ddb74590fbda80a273a46e9c75fb7
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-05-24 12:02:15 +0200

    Remise à jour du venv

--- beeware-venv/Scripts/activate.bat
+++ beeware-venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\killian\Desktop\appLSLVisu\beeware-venv
+set VIRTUAL_ENV=C:\Users\killian\PycharmProjects\saveBeeware\beeware-venv
 
 if not defined PROMPT set PROMPT=$P$G
 
