b8237296221b839c4b9e3c191f1f879d57fecf1c
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-07-05 09:39:35 +0200

    Ajout des wheel pour build en android

--- appvisulsl/src/appvisulsl/resources/wheel/pylsl/test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-class TestPyLSL(unittest.TestCase):
-
-    def test_pylsl(self):
-        from pylsl import StreamOutlet
-        self.assertEqual(type(StreamOutlet(name='myStream', type='EEG', channel_count=8)), "<class 'pylsl.pylsl.StreamInfo'>")
